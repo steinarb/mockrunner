2d111f8e87ec0eae13ea03645e902073cac1e887
aibba <aibba>
2003-08-04 16:35:28 +0000

    *** empty log message ***

--- src/com/mockrunner/example/servlet/RedirectServletTest.java
+++ src/com/mockrunner/example/servlet/RedirectServletTest.java
@@ -2,12 +2,6 @@ package com.mockrunner.example.servlet;
 
 import java.io.BufferedReader;
 
-import org.cyberneko.html.parsers.DOMParser;
-import org.jdom.Document;
-import org.jdom.input.DOMBuilder;
-import org.jdom.input.SAXBuilder;
-import org.xml.sax.InputSource;
-
 import com.mockrunner.servlet.ServletTestCaseAdapter;
 
 /**
