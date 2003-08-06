e41134cf8d14acc9b772e0df0d93f1d298c5b9a2
aibba <aibba>
2003-08-06 18:26:24 +0000

    *** empty log message ***

--- src/com/mockrunner/mock/MockFilterConfig.java
+++ src/com/mockrunner/mock/MockFilterConfig.java
@@ -2,6 +2,9 @@ package com.mockrunner.mock;
 
 import java.util.Enumeration;
 
+/**
+ * Mock implementation of <code>FilterConfig</code>.
+ */
 public class MockFilterConfig extends com.mockobjects.servlet.MockFilterConfig
 {
     public String getInitParameter(String key)
