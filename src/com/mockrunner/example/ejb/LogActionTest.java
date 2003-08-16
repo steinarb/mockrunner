8e8696ee961caa1d7e8df6ef70db0072f58f5405
aibba <aibba>
2003-08-16 11:30:33 +0000

    *** empty log message ***

--- src/com/mockrunner/example/ejb/LogActionTest.java
+++ src/com/mockrunner/example/ejb/LogActionTest.java
@@ -6,13 +6,17 @@ import org.mockejb.SessionBeanDescriptor;
 
 import com.mockrunner.example.ejb.interfaces.LogSession;
 import com.mockrunner.example.ejb.interfaces.LogSessionHome;
+import com.mockrunner.jdbc.JDBCTestModule;
 import com.mockrunner.struts.ActionTestCaseAdapter;
 
 public class LogActionTest extends ActionTestCaseAdapter
 {
+    private JDBCTestModule jdbcModule;
+    
     protected void setUp() throws Exception
     {
         super.setUp();
+        jdbcModule = createJDBCTestModule();
         SessionBeanDescriptor beanDescriptor = new SessionBeanDescriptor("com/mockrunner/example/LogSession", LogSessionHome.class, LogSession.class, LogSessionBean.class);
         MockContainer.deploy(beanDescriptor);
         MockContext.add("java:/DefaultDS", getMockObjectFactory().getMockDataSource());
@@ -22,6 +26,8 @@ public class LogActionTest extends ActionTestCaseAdapter
     {
         getMockObjectFactory().getMockRequest().setupAddParameter("message", "testmessage");
         actionPerform(LogAction.class);
+        jdbcModule.verifyPreparedStatementPresent("insert into logtable values(?, ?, ?)");
+        jdbcModule.verifyPreparedStatementObject("insert into logtable values(?, ?, ?)", 3, "testmessage");
     }
     
 }
