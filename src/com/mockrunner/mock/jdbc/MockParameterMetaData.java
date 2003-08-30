27f911928770402338b4440d3f55a75e480c841e
aibba <aibba>
2003-08-30 07:48:07 +0000

    *** empty log message ***

--- src/com/mockrunner/mock/jdbc/MockParameterMetaData.java
+++ src/com/mockrunner/mock/jdbc/MockParameterMetaData.java
@@ -12,71 +12,15 @@ import java.util.Map;
 public class MockParameterMetaData implements ParameterMetaData
 {
     private int parameterCount;
-    private Map parameterMode;
-    private Map parameterType;
-    private Map precision;
-    private Map scale;
-    private Map nullable;
-    private Map signed;
-    private Map parameterClassName;
-    private Map parameterTypeName;
     
     public MockParameterMetaData()
     {
         parameterCount = 0;
-        parameterMode = new HashMap();
-        parameterType = new HashMap();
-        precision = new HashMap();
-        scale = new HashMap();
-        nullable = new HashMap();
-        signed = new HashMap();
-        parameterClassName = new HashMap();
-        parameterTypeName = new HashMap();
     }
     
     public void setupParameterCount(int count)
     {
         parameterCount = count;
-    }
-    
-    public void setupParameterMode(int param, int mode)
-    {
-        parameterMode.put(new Integer(param), new Integer(mode));
-    }
-    
-    public void setupParameterType(int param, int type)
-    {
-        parameterType.put(new Integer(param), new Integer(type));
-    }
-    
-    public void setupPrecision(int param, int prec)
-    {
-        precision.put(new Integer(param), new Integer(prec));
-    }
-    
-    public void setupScale(int param, int paramScale)
-    {
-        scale.put(new Integer(param), new Integer(paramScale));
-    }
-    
-    public void setupIsNullable(int param, int isNullable)
-    {
-        nullable.put(new Integer(param), new Integer(isNullable));
-    }
-    
-    public void setupIsSigned(int param, boolean isSigned)
-    {
-        signed.put(new Integer(param), new Boolean(isSigned));
-    }
-    
-    public void setupParameterClassName(int param, String className)
-    {
-        parameterClassName.put(new Integer(param), className);
-    }
-    
-    public void setupParameterTypeName(int param, String typeName)
-    {
-        parameterTypeName.put(new Integer(param), typeName);
     }
     
     public int getParameterCount() throws SQLException
@@ -86,58 +30,42 @@ public class MockParameterMetaData implements ParameterMetaData
 
     public int getParameterMode(int param) throws SQLException
     {
-        Integer mode = (Integer)parameterMode.get(new Integer(param));
-        if(null == mode) return ParameterMetaData.parameterModeUnknown;
-        return mode.intValue();
+        return ParameterMetaData.parameterModeUnknown;
     }
 
     public int getParameterType(int param) throws SQLException
     {
-        Integer type = (Integer)parameterType.get(new Integer(param));
-        if(null == type) return Types.OTHER;
-        return type.intValue();
+        return Types.OTHER;
     }
 
     public int getPrecision(int param) throws SQLException
     {
-        Integer prec = (Integer)precision.get(new Integer(param));
-        if(null == prec) return 0;
-        return prec.intValue();
+        return 0;
     }
 
     public int getScale(int param) throws SQLException
     {
-        Integer paramScale = (Integer)scale.get(new Integer(param));
-        if(null == paramScale) return 0;
-        return paramScale.intValue();
+        return 0;
     }
 
     public int isNullable(int param) throws SQLException
     {
-        Integer isNullable = (Integer)nullable.get(new Integer(param));
-        if(null == isNullable) return ParameterMetaData.parameterNullable;
-        return isNullable.intValue();
+        return ParameterMetaData.parameterNullable;
     }
 
     public boolean isSigned(int param) throws SQLException
     {
-        Boolean isSigned = (Boolean)signed.get(new Integer(param));
-        if(null == isSigned) return false;
-        return isSigned.booleanValue();
+        return false;
     }
 
     public String getParameterClassName(int param) throws SQLException
     {
-        String className = (String)parameterClassName.get(new Integer(param));
-        if(null == className) return Object.class.getName();
-        return className;
+        return Object.class.getName();
     }
 
     public String getParameterTypeName(int param) throws SQLException
     {
-        String typeName = (String)parameterTypeName.get(new Integer(param));
-        if(null == typeName) return "";
-        return typeName;
+        return "";
     }
 
 }
