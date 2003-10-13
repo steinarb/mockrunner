2fffa871c495f44fbe2047b70ec574b5f5c550c7
aibba <aibba>
2003-10-13 09:46:05 +0000

    *** empty log message ***

---
+++ src/com/mockrunner/mock/web/MockSessionContext.java
@@ -0,0 +1,23 @@
+package com.mockrunner.mock.web;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionContext;
+
+/**
+ * Mock implementation of <code>HttpSessionContext</code>.
+ */
+public class MockSessionContext implements HttpSessionContext
+{
+    public Enumeration getIds()
+    { 
+        return new Vector().elements();
+    }
+
+    public HttpSession getSession(String arg0)
+    {
+        return null;
+    }
+}
