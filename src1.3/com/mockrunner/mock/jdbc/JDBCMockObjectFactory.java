799ccf22d399d20d1b5e756ff61b8f049f993765
aibba <aibba>
2003-10-15 08:42:07 +0000

    *** empty log message ***

--- src1.3/com/mockrunner/mock/jdbc/JDBCMockObjectFactory.java
+++ src1.3/com/mockrunner/mock/jdbc/JDBCMockObjectFactory.java
@@ -18,7 +18,6 @@ public class JDBCMockObjectFactory
     private MockDataSource dataSource;
     private MockDriver driver;
     private MockConnection connection;
-    private MockUserTransaction transaction;
     
     /**
      * Creates a new set of mock objects.
@@ -28,7 +27,6 @@ public class JDBCMockObjectFactory
         dataSource = new MockDataSource();
         driver = new MockDriver();
         connection = new MockConnection();
-        transaction = new MockUserTransaction();
         setUpDependencies();
     }
     
@@ -76,14 +74,5 @@ public class JDBCMockObjectFactory
     public MockConnection getMockConnection()
     {
         return connection;
-    }
-    
-    /**
-     * Returns the {@link com.mockrunner.mock.jdbc.MockUserTransaction}.
-     * @return the {@link com.mockrunner.mock.jdbc.MockUserTransaction}
-     */
-    public MockUserTransaction getMockUserTransaction()
-    {
-        return transaction;
     }
 }
