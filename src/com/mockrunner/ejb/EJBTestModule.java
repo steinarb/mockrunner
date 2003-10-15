259ceffb534b63100ceb32c8ff3dcb5e5eb6d4c1
aibba <aibba>
2003-10-15 09:49:21 +0000

    *** empty log message ***

--- src/com/mockrunner/ejb/EJBTestModule.java
+++ src/com/mockrunner/ejb/EJBTestModule.java
@@ -1,5 +1,8 @@
 package com.mockrunner.ejb;
 
+import org.mockejb.MockContainer;
+import org.mockejb.SessionBeanDescriptor;
+
 import com.mockrunner.mock.ejb.EJBMockObjectFactory;
 
 /**
@@ -8,9 +11,53 @@ import com.mockrunner.mock.ejb.EJBMockObjectFactory;
 public class EJBTestModule
 {
     private EJBMockObjectFactory mockFactory;
+    private String impSuffix;
+    private String homeSuffix;
+    private String remoteSuffix;
+    private String homeInterfacePackage;
+    private String remoteInterfacePackage;
     
     public EJBTestModule(EJBMockObjectFactory mockFactory)
     {
         this.mockFactory = mockFactory;
+        impSuffix = "Bean";
+        homeSuffix = "Home";
+        remoteSuffix = "";
+    }
+    
+    public void setImplementationSuffix(String impSuffix)
+    {
+        this.impSuffix = impSuffix;
+    }
+    
+    public void setRemoteSuffix(String remoteSuffix)
+    {
+        this.remoteSuffix = remoteSuffix;
+    }
+    
+    public void setHomeSuffix(String homeSuffix)
+    {
+        this.homeSuffix = homeSuffix;
+    }
+    
+    public void setInterfacePackage(String interfacePackage)
+    {
+        setHomeInterfacePackage(interfacePackage);
+        setRemoteInterfacePackage(interfacePackage);
+    }
+    
+    public void setHomeInterfacePackage(String homeInterfacePackage)
+    {
+        this.homeInterfacePackage = homeInterfacePackage;
+    }
+    
+    public void setRemoteInterfacePackage(String remoteInterfacePackage)
+    {
+        this.remoteInterfacePackage = remoteInterfacePackage;
+    }
+    
+    public void deploy(SessionBeanDescriptor descriptor)
+    {
+        MockContainer.deploy(descriptor);
     }
 }
