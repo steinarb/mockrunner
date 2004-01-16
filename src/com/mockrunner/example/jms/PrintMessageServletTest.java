12d09059c630f0e66fa5df556d25d04d80f14296
aibba <aibba>
2004-01-16 20:59:20 +0000

    *** empty log message ***

--- src/com/mockrunner/example/jms/PrintMessageServletTest.java
+++ src/com/mockrunner/example/jms/PrintMessageServletTest.java
@@ -1,8 +1,20 @@
 package com.mockrunner.example.jms;
 
-import junit.framework.TestCase;
+import com.mockrunner.ejb.EJBTestModule;
+import com.mockrunner.jms.JMSTestCaseAdapter;
+import com.mockrunner.servlet.ServletTestModule;
 
-public class PrintMessageServletTest extends TestCase
+public class PrintMessageServletTest extends JMSTestCaseAdapter
 {
+    private EJBTestModule ejbModule;
+    private ServletTestModule servletModule;
+    
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        ejbModule = new EJBTestModule(createEJBMockObjectFactory());
+        servletModule = new ServletTestModule(createWebMockObjectFactory());
+        servletModule.createServlet(PrintMessageServlet.class);
+    }
 
 }
