7309ea9d27489ebffd2e573de419831a0becf533
aibba <aibba>
2004-01-16 19:13:59 +0000

    *** empty log message ***

--- src/com/mockrunner/mock/jms/MockTopicConnectionFactory.java
+++ src/com/mockrunner/mock/jms/MockTopicConnectionFactory.java
@@ -1,28 +1,70 @@
 package com.mockrunner.mock.jms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jms.JMSException;
 import javax.jms.TopicConnection;
 import javax.jms.TopicConnectionFactory;
+
+import com.mockrunner.jms.DestinationManager;
 
 /**
  * Mock implementation of JMS <code>TopicConnectionFactory</code>.
  */
 public class MockTopicConnectionFactory implements TopicConnectionFactory
 {
-    private TopicConnection topicConnection;
+    private DestinationManager destinationManager;
+    private List topicConnections;
     
-    public void setTopicConnection(TopicConnection topicConnection)
+    public MockTopicConnectionFactory(DestinationManager destinationManager)
     {
-        this.topicConnection = topicConnection;
+        this.destinationManager = destinationManager;
+        topicConnections = new ArrayList();
     }
     
     public TopicConnection createTopicConnection() throws JMSException
     {
-        return topicConnection;
+        TopicConnection connection = new MockTopicConnection(destinationManager);
+        topicConnections.add(connection);
+        return connection;
     }
 
     public TopicConnection createTopicConnection(String name, String password) throws JMSException
     {
-        return topicConnection;
+        return createTopicConnection();
+    }
+    
+    /**
+     * Clears the list of connections
+     */
+    public void clearConnections()
+    {
+        topicConnections.clear();
+    }
+
+    /**
+     * Returns the connection with the specified index
+     * resp. <code>null</code> if no such connection
+     * exists.
+     * @param index the index
+     * @return the connection
+     */
+    public MockTopicConnection getTopicConnection(int index)
+    {
+        if(topicConnections.size() <= index) return null;
+        return (MockTopicConnection)topicConnections.get(index);
+    }
+
+    /**
+     * Returns the latest created connection
+     * resp. <code>null</code> if no such connection
+     * exists.
+     * @return the connection
+     */
+    public MockTopicConnection getLatestTopicConnection()
+    {
+        if(topicConnections.size() == 0) return null;
+        return (MockTopicConnection)topicConnections.get(topicConnections.size() - 1);
     }
 }
