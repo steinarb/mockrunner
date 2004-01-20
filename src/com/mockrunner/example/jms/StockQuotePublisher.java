f73ce2d2bab11b22c1dfed26c18f12226dee45cd
aibba <aibba>
2004-01-20 20:48:57 +0000

    *** empty log message ***

---
+++ src/com/mockrunner/example/jms/StockQuotePublisher.java
@@ -0,0 +1,98 @@
+package com.mockrunner.example.jms;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import javax.jms.TopicConnection;
+import javax.jms.TopicConnectionFactory;
+import javax.jms.TopicPublisher;
+import javax.jms.TopicSession;
+import javax.naming.InitialContext;
+
+public class StockQuotePublisher
+{
+    private Map nasdaqRates = new HashMap();
+    private Map dowRates = new HashMap();
+    
+    public void setQuotes(Map nasdaqRates, Map dowRates)
+    {
+        this.nasdaqRates.clear();
+        this.dowRates.clear();
+        this.nasdaqRates.putAll(nasdaqRates);
+        this.dowRates.putAll(dowRates);
+    }
+    
+    public void send()
+    {
+        TopicConnection topicConnection = null;
+        TopicSession topicSession = null;
+        TopicPublisher topicPublisher = null;
+        try
+        {   
+            InitialContext initialContext = new InitialContext();
+            TopicConnectionFactory topicFactory = (TopicConnectionFactory)initialContext.lookup("java:/ConnectionFactory");
+            topicConnection = topicFactory.createTopicConnection();
+            topicSession = topicConnection.createTopicSession(true, Session.AUTO_ACKNOWLEDGE);
+            Topic topic = (Topic)initialContext.lookup("topic/quoteTopic");
+            TextMessage timeMessage = createInitialTimestampMessage(topicSession);
+            MapMessage nasdaqMessage = createStockQuoteMessage(topicSession, nasdaqRates);
+            MapMessage dowMessage = createStockQuoteMessage(topicSession, dowRates);
+            nasdaqMessage.setJMSCorrelationID(timeMessage.getText());
+            dowMessage.setJMSCorrelationID(timeMessage.getText());
+            topicPublisher = topicSession.createPublisher(topic);
+            topicPublisher.publish(timeMessage);
+            topicPublisher.publish(nasdaqMessage);
+            topicPublisher.publish(dowMessage);
+            topicSession.commit();
+        }
+        catch(Exception exc)
+        {
+            try
+            {
+                if(null != topicSession) topicSession.rollback();
+            }
+            catch(JMSException jmsExc)
+            {
+                jmsExc.printStackTrace();
+            }
+        }
+        finally
+        {
+            try
+            {
+                if(null != topicPublisher) topicPublisher.close();
+                if(null != topicSession) topicSession.close();
+                if(null != topicConnection) topicConnection.close();
+            }
+            catch(JMSException exc)
+            {
+                exc.printStackTrace();
+            }
+        }
+    }
+    
+    private TextMessage createInitialTimestampMessage(TopicSession topicSession) throws JMSException
+    {
+        TextMessage message = topicSession.createTextMessage();
+        message.setText(String.valueOf(System.currentTimeMillis()));
+        return message;
+    }
+
+    private MapMessage createStockQuoteMessage(TopicSession topicSession, Map rates) throws JMSException
+    {
+        MapMessage message = topicSession.createMapMessage();
+        Iterator keys = rates.keySet().iterator();
+        while(keys.hasNext())
+        {
+            String nextKey = (String)keys.next();
+            message.setString(nextKey, (String)rates.get(nextKey));
+        }
+        return message;
+    }
+}
---
+++ src/com/mockrunner/example/jms/StockQuotePublisherTest.java
@@ -0,0 +1,66 @@
+package com.mockrunner.example.jms;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.jms.MapMessage;
+import javax.jms.TextMessage;
+
+import com.mockrunner.ejb.EJBTestModule;
+import com.mockrunner.jms.JMSTestCaseAdapter;
+import com.mockrunner.mock.jms.MockTopic;
+
+public class StockQuotePublisherTest extends JMSTestCaseAdapter
+{
+    private EJBTestModule ejbModule;
+    private MockTopic topic;
+    private StockQuotePublisher sender;
+    
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        ejbModule = new EJBTestModule(createEJBMockObjectFactory());
+        ejbModule.bindToContext("java:/ConnectionFactory", getJMSMockObjectFactory().getMockTopicConnectionFactory());
+        topic = getDestinationManager().createTopic("testTopic");
+        ejbModule.bindToContext("topic/quoteTopic", topic);
+        sender = new StockQuotePublisher();
+    }
+    
+    private void prepareTestRates(Map nasdaqRates, Map dowRates)
+    {
+        nasdaqRates.put("NasdaqCompany1", "10.3");
+        nasdaqRates.put("NasdaqCompany2", "30.1");
+        dowRates.put("DowJonesCompany1", "11.5");
+    }
+
+    public void testClosedAndCommitted()
+    {
+        sender.send();
+        verifyNumberTopicSessions(1);
+        verifyNumberTopicPublishers(0, "testTopic", 1);
+        verifyAllTopicSessionsClosed();
+        verifyAllTopicPublishersClosed(0);
+        verifyTopicConnectionClosed();
+        verifyAllTopicSessionsCommitted();
+        verifyNumberOfReceivedTopicMessages("testTopic", 3);
+    }
+    
+    public void testVerifyReceivedMessages() throws Exception
+    {
+        Map nasdaqRates = new HashMap();
+        Map dowRates = new HashMap();
+        prepareTestRates(nasdaqRates, dowRates);
+        sender.setQuotes(nasdaqRates, dowRates);
+        sender.send();
+        List receivedMessages = getReceivedMessageListFromTopic("testTopic");
+        TextMessage timeMessage = (TextMessage)receivedMessages.get(0);
+        MapMessage nasdaqMessage = (MapMessage)receivedMessages.get(1);
+        MapMessage dowMessage = (MapMessage)receivedMessages.get(2);
+        assertEquals(timeMessage.getText(), nasdaqMessage.getJMSCorrelationID());
+        assertEquals(timeMessage.getText(), dowMessage.getJMSCorrelationID());
+        assertEquals("10.3", nasdaqMessage.getString("NasdaqCompany1"));
+        assertEquals("30.1", nasdaqMessage.getString("NasdaqCompany2"));
+        assertEquals("11.5", dowMessage.getString("DowJonesCompany1"));
+    }
+}
