0826968520ed6c15bcdee520ffb79f1af1cd9ff4
aibba <aibba>
2004-02-28 13:27:11 +0000

    *** empty log message ***

--- src/com/mockrunner/example/struts/GreetingsActionTest.java
+++ src/com/mockrunner/example/struts/GreetingsActionTest.java
@@ -1,31 +1,62 @@
 package com.mockrunner.example.struts;
 
+import org.apache.commons.validator.ValidatorResources;
+
 import com.mockrunner.struts.ActionTestCaseAdapter;
 import com.mockrunner.struts.MapMessageResources;
 
 /**
  * Example test for the {@link GreetingsAction}.
+ * Please note that we cache the <code>ValidatorResources</code>
+ * in a static field. You don't need to do this, but the parsing
+ * of the files before every test method will slow down your tests.
  */
 public class GreetingsActionTest extends ActionTestCaseAdapter
 {
+    private static ValidatorResources validatorRes = null;
+    
     protected void setUp() throws Exception
     {
         super.setUp();
+        if(null == validatorRes)
+        {
+            String[] files = new String[2];
+            files[0] = "src/com/mockrunner/example/struts/validator-rules.xml";
+            files[1] = "src/com/mockrunner/example/struts/validation.xml";
+            validatorRes = createValidatorResources(files);
+        }
+        setValidatorResources(validatorRes);
         MapMessageResources resources = new MapMessageResources();
         resources.putMessages("src/com/mockrunner/example/struts/Application.properties");
         setResources(resources);
+        setValidate(true);
     }
     
-    public void testGreetings()
+    public void testSuccesfulGreetings()
     {
         getWebMockObjectFactory().getMockServletContext().setAttribute("counter", new Integer(0));
+        getWebMockObjectFactory().getMockActionMapping().setPath("/greetings");
         addRequestParameter("name", "testname");
-        actionPerform(GreetingsAction.class);
+        actionPerform(GreetingsAction.class, GreetingsValidatorForm.class);
         assertEquals("Hello testname, you are visitor 1", getWebMockObjectFactory().getMockRequest().getAttribute("greetings"));
         getWebMockObjectFactory().getMockServletContext().setAttribute("counter", new Integer(6));
-        actionPerform(GreetingsAction.class);
+        verifyNoActionErrors();
+        actionPerform(GreetingsAction.class, GreetingsValidatorForm.class);
         assertEquals("Hello testname, you are visitor 7", getWebMockObjectFactory().getMockRequest().getAttribute("greetings"));
         verifyNoActionErrors();
         verifyForward("success");
+    }
+    
+    public void testValidationError()
+    {
+        getWebMockObjectFactory().getMockServletContext().setAttribute("counter", new Integer(0));
+        getWebMockObjectFactory().getMockActionMapping().setPath("/greetings"); 
+        actionPerform(GreetingsAction.class, GreetingsValidatorForm.class);
+        verifyNumberActionErrors(1);
+        verifyActionErrorPresent("errors.required");
+        addRequestParameter("name", "y");
+        actionPerform(GreetingsAction.class, GreetingsValidatorForm.class);
+        verifyNumberActionErrors(1);
+        verifyActionErrorPresent("errors.minlength");
     }
 }
