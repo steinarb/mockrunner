b8770f6de6d7f5fbf5a017d8cea354892396f5f1
aibba <aibba>
2004-12-15 20:02:52 +0000

    *** empty log message ***

--- src/com/mockrunner/mock/jdbc/MockDataSource.java
+++ src/com/mockrunner/mock/jdbc/MockDataSource.java
@@ -15,11 +15,31 @@ public class MockDataSource implements DataSource
     private int loginTimeout = 0;
     private PrintWriter logWriter = null;
     
+    /**
+     * Set up the connection.
+     * @param connection the connection
+     */
     public void setupConnection(Connection connection)
     {
         this.connection = connection;
     }
     
+    /**
+     * Returns the {@link com.mockrunner.mock.jdbc.MockConnection}. 
+     * If the underlying connection is not an instance of 
+     * {@link com.mockrunner.mock.jdbc.MockConnection},
+     * <code>null</code> is returned.
+     * @return the {@link com.mockrunner.mock.jdbc.MockConnection}
+     */
+    public MockConnection getMockConnection()
+    {
+        if(connection instanceof MockConnection)
+        {
+            return (MockConnection)connection;
+        }
+        return null;
+    }
+
     public int getLoginTimeout() throws SQLException
     {
         return loginTimeout;
