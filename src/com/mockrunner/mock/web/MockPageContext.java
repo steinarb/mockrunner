74713e1c144e976a3ff7fae497080958aef6fdca
aibba <aibba>
2005-01-15 14:00:52 +0000

    *** empty log message ***

--- src/com/mockrunner/mock/web/MockPageContext.java
+++ src/com/mockrunner/mock/web/MockPageContext.java
@@ -1,35 +1,56 @@
 package com.mockrunner.mock.web;
 
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Stack;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.servlet.jsp.JspWriter;
-//import javax.servlet.jsp.el.ExpressionEvaluator;
-//import javax.servlet.jsp.el.VariableResolver;
+import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.BodyContent;
 
 /**
  * Mock implementation of <code>PageContext</code>.
  */
-public class MockPageContext extends org.apache.struts.mock.MockPageContext
+//Some methods of this class were copied from org.apache.struts.mock.MockPageContext
+//and modified
+public class MockPageContext extends PageContext
 {
+    protected ServletConfig config;
+    protected ServletRequest request;
+    protected ServletResponse response;
     private JspWriter jspWriter;
     private Stack outStack;
     private Exception exception;
     private Object page;
+    private HashMap attributes;
     //private ExpressionEvaluator evaluator;
     //private VariableResolver resolver;
+    
+    public MockPageContext()
+    {
+        this(null, null, null);
+    }
 
     public MockPageContext(ServletConfig config, ServletRequest request, ServletResponse response)
     {
-        super(config, request, response);
+        this.config = config;
+        this.request = request;
+        this.response = response;
         jspWriter = new MockJspWriter();
         outStack = new Stack();
+        attributes = new HashMap();
     }
     
     /**
@@ -42,7 +63,211 @@ public class MockPageContext extends org.apache.struts.mock.MockPageContext
     {
         this.jspWriter = jspWriter;
     }
-  
+    
+    public void setPage(Object page) 
+    {
+        this.page = page;
+    }
+    
+    public void setServletConfig(ServletConfig config)
+    {
+        this.config = config;
+    }
+    
+    public void setServletRequest(ServletRequest request)
+    {
+        this.request = request;
+    }
+    
+    public void setServletResponse(ServletResponse response)
+    {
+        this.response = response;
+    }
+    
+    public void setException(Exception exception) 
+    {
+        this.exception = exception;
+    }
+    
+    public Object findAttribute(String name) 
+    {
+        Object value = getAttribute(name, PageContext.PAGE_SCOPE);
+        if(value == null) 
+        {
+            value = getAttribute(name, PageContext.REQUEST_SCOPE);
+        }
+        if(value == null) 
+        {
+            value = getAttribute(name, PageContext.SESSION_SCOPE);
+        }
+        if(value == null) 
+        {
+            value = getAttribute(name, PageContext.APPLICATION_SCOPE);
+        }
+        return value;
+    }
+    
+    public Object getAttribute(String name) 
+    {
+        return getAttribute(name, PageContext.PAGE_SCOPE);
+    }
+
+    public Object getAttribute(String name, int scope) 
+    {
+        if(scope == PageContext.PAGE_SCOPE) 
+        {
+            return attributes.get(name);
+        } 
+        else if(scope == PageContext.REQUEST_SCOPE) 
+        {
+            if(null == request) return null;
+            return request.getAttribute(name);
+        } 
+        else if(scope == PageContext.SESSION_SCOPE) 
+        {
+            if(null == getSession()) return null;
+            return getSession().getAttribute(name);
+        } 
+        else if(scope == PageContext.APPLICATION_SCOPE) 
+        {
+            if(null == getServletContext()) return null;
+            return getServletContext().getAttribute(name);
+        } 
+        else 
+        {
+            throw new IllegalArgumentException("Invalid scope " + scope);
+        }
+    }
+    
+    public void removeAttribute(String name) 
+    {
+        int scope = getAttributesScope(name);
+        if (scope != 0) 
+        {
+            removeAttribute(name, scope);
+        }
+    }
+
+    public void removeAttribute(String name, int scope) 
+    {
+        if(scope == PageContext.PAGE_SCOPE) 
+        {
+            attributes.remove(name);
+        } 
+        else if(scope == PageContext.REQUEST_SCOPE) 
+        {
+            if(request != null) 
+            {
+                request.removeAttribute(name);
+            }
+        } 
+        else if(scope == PageContext.SESSION_SCOPE) 
+        {
+            if(getSession() != null) 
+            {
+                getSession().removeAttribute(name);
+            }
+        } 
+        else if(scope == PageContext.APPLICATION_SCOPE) 
+        {
+            if(getServletContext() != null) 
+            {
+                getServletContext().removeAttribute(name);
+            }
+        } 
+        else 
+        {
+            throw new IllegalArgumentException("Invalid scope " + scope);
+        }
+    }
+    
+    public void setAttribute(String name, Object value) 
+    {
+        setAttribute(name, value, PageContext.PAGE_SCOPE);
+    }
+
+
+    public void setAttribute(String name, Object value, int scope) 
+    {
+        if(scope == PageContext.PAGE_SCOPE) 
+        {
+            attributes.put(name, value);
+        } 
+        else if(scope == PageContext.REQUEST_SCOPE) 
+        {
+            if(request != null) 
+            {
+                request.setAttribute(name, value);
+            }
+        } 
+        else if(scope == PageContext.SESSION_SCOPE) 
+        {
+            if(getSession() != null) 
+            {
+                getSession().setAttribute(name, value);
+            }
+        } 
+        else if(scope == PageContext.APPLICATION_SCOPE) 
+        {
+            if(getServletContext() != null) 
+            {
+                getServletContext().setAttribute(name, value);
+            }
+        } 
+        else 
+        {
+            throw new IllegalArgumentException("Invalid scope " + scope);
+        }
+    }
+    
+    public int getAttributesScope(String name) 
+    {
+        if(getAttribute(name, PageContext.PAGE_SCOPE) != null) 
+        {
+            return PageContext.PAGE_SCOPE;
+        } 
+        else if(getAttribute(name, PageContext.REQUEST_SCOPE) != null) 
+        {
+            return PageContext.REQUEST_SCOPE;
+        } 
+        else if(getAttribute(name, PageContext.SESSION_SCOPE) != null)
+        {
+            return PageContext.SESSION_SCOPE;
+        } 
+        else if(getAttribute(name, PageContext.APPLICATION_SCOPE) != null) 
+        {
+            return PageContext.APPLICATION_SCOPE;
+        } 
+        return 0;
+    }
+    
+    public Enumeration getAttributeNamesInScope(int scope) 
+    {
+        if(scope == PageContext.PAGE_SCOPE) 
+        {
+            return new WrappedEnumeration(attributes.keySet().iterator());
+        } 
+        else if(scope == PageContext.REQUEST_SCOPE) 
+        {
+            if(request == null) return new NullEnumeration();
+            return request.getAttributeNames();
+        } 
+        else if(scope == PageContext.SESSION_SCOPE) 
+        {
+            if(getSession() == null) return new NullEnumeration();
+            return getSession().getAttributeNames();
+        } 
+        else if(scope == PageContext.APPLICATION_SCOPE) 
+        {
+            if(getServletContext() == null) return new NullEnumeration();
+            return getServletContext().getAttributeNames();
+        } 
+        else 
+        {
+            throw new IllegalArgumentException("Invalid scope " + scope);
+        }
+    }
+    
     public JspWriter getOut()
     {
         return jspWriter;
@@ -53,19 +278,37 @@ public class MockPageContext extends org.apache.struts.mock.MockPageContext
         return exception;
     }
     
-    public void setException(Exception exception) 
-    {
-        this.exception = exception;
-    }
-    
     public Object getPage() 
     {
         return page;
     }
-    
-    public void setPage(Object page) 
+
+    public ServletRequest getRequest() 
     {
-        this.page = page;
+        return request;
+    }
+
+    public ServletResponse getResponse() 
+    {
+        return response;
+    }
+
+    public ServletConfig getServletConfig() 
+    {
+        return config;
+    }
+
+    public ServletContext getServletContext() 
+    {
+        if(null == config) return null;
+        return config.getServletContext();
+    }
+
+
+    public HttpSession getSession() 
+    {
+        if(null == request) return null;
+        return ((HttpServletRequest)request).getSession();
     }
     
     public void handlePageException(Exception exc) 
@@ -78,19 +321,37 @@ public class MockPageContext extends org.apache.struts.mock.MockPageContext
     
     }
     
-    public void forward(String path) 
+    public void forward(String path) throws ServletException, IOException
     {
-    
+        if(null != request)
+        {
+            RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+            if(null != dispatcher)
+            {
+                dispatcher.forward(request, response); 
+            }
+        }
     }
 
-    public void include(String path)
+    public void include(String path) throws ServletException, IOException
     {
-    
+        if(null != request)
+        {
+            RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+            if(null != dispatcher)
+            {
+                dispatcher.include(request, response); 
+            }
+        }
     }
     
     public void include(String path, boolean flush) throws ServletException, IOException
     {
-
+        if(flush)
+        {
+            jspWriter.flush();
+        }
+        include(path);
     }
 
     public void initialize(Servlet servlet, ServletRequest request,
@@ -98,7 +359,12 @@ public class MockPageContext extends org.apache.struts.mock.MockPageContext
                            boolean needsSession, int bufferSize,
                            boolean autoFlush) 
     {
-        
+        this.config = servlet.getServletConfig();
+        this.request = request;
+        this.response = response;
+        jspWriter = new MockJspWriter();
+        outStack = new Stack();
+        attributes = new HashMap();
     }
 
     public JspWriter popBody() 
@@ -110,9 +376,8 @@ public class MockPageContext extends org.apache.struts.mock.MockPageContext
     public BodyContent pushBody() 
     {
         outStack.push(jspWriter);
-        MockBodyContent bodyContent = new MockBodyContent(jspWriter);
-        jspWriter = bodyContent;
-        return bodyContent;
+        jspWriter = new MockBodyContent(jspWriter);;
+        return (BodyContent)jspWriter;
     }
 
     public void release() 
@@ -140,4 +405,37 @@ public class MockPageContext extends org.apache.struts.mock.MockPageContext
     {
         return resolver;
     }*/
+    
+    private class NullEnumeration implements Enumeration 
+    {
+        public boolean hasMoreElements() 
+        {
+            return false;
+        }
+
+        public Object nextElement() 
+        {
+            throw new NoSuchElementException();
+        }
+    }
+    
+    private class WrappedEnumeration implements Enumeration 
+    {
+        private Iterator iterator;
+        
+        public WrappedEnumeration(Iterator iterator) 
+        {
+            this.iterator = iterator;
+        }
+
+        public boolean hasMoreElements() 
+        {
+            return iterator.hasNext();
+        }
+
+        public Object nextElement() 
+        {
+            return iterator.next();
+        }
+    }
 }
--- src/com/mockrunner/test/web/MockPageContextTest.java
+++ src/com/mockrunner/test/web/MockPageContextTest.java
@@ -1,13 +1,20 @@
 package com.mockrunner.test.web;
 
+import java.io.IOException;
 import java.io.StringWriter;
+import java.util.Enumeration;
 
 import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
 
 import com.mockrunner.base.BaseTestCase;
 import com.mockrunner.mock.web.MockBodyContent;
+import com.mockrunner.mock.web.MockHttpServletRequest;
+import com.mockrunner.mock.web.MockHttpServletResponse;
 import com.mockrunner.mock.web.MockJspWriter;
 import com.mockrunner.mock.web.MockPageContext;
+import com.mockrunner.mock.web.MockRequestDispatcher;
+import com.mockrunner.mock.web.MockServletConfig;
 
 public class MockPageContextTest extends BaseTestCase
 {
@@ -40,5 +47,259 @@ public class MockPageContextTest extends BaseTestCase
         pageContext.popBody();
         writer = pageContext.getOut();
         assertEquals("TestOut", ((MockJspWriter)writer).getOutputAsString());    
+    }
+    
+    public void testGetAttribute() throws Exception
+    {
+        pageContext.setAttribute("pageKey", "pageValue");
+        getWebMockObjectFactory().getMockRequest().setAttribute("requestKey", "requestValue");
+        getWebMockObjectFactory().getMockSession().setAttribute("sessionKey", "sessionValue");
+        getWebMockObjectFactory().getMockServletContext().setAttribute("contextKey", "contextValue");
+        assertEquals("pageValue", pageContext.getAttribute("pageKey"));
+        assertEquals("pageValue", pageContext.getAttribute("pageKey", PageContext.PAGE_SCOPE));
+        assertEquals("requestValue", pageContext.getAttribute("requestKey", PageContext.REQUEST_SCOPE));
+        assertEquals("sessionValue", pageContext.getAttribute("sessionKey", PageContext.SESSION_SCOPE));
+        assertEquals("contextValue", pageContext.getAttribute("contextKey", PageContext.APPLICATION_SCOPE));
+        assertNull(pageContext.getAttribute("pageKey1"));
+        assertNull(pageContext.getAttribute("pageKey1", PageContext.PAGE_SCOPE));
+        assertNull(pageContext.getAttribute("requestKey1", PageContext.REQUEST_SCOPE));
+        assertNull(pageContext.getAttribute("sessionKey1", PageContext.SESSION_SCOPE));
+        assertNull(pageContext.getAttribute("contextKey1", PageContext.APPLICATION_SCOPE));
+        pageContext.setServletRequest(null);
+        assertNull(pageContext.getAttribute("requestKey", PageContext.REQUEST_SCOPE));
+        assertNull(pageContext.getAttribute("sessionKey", PageContext.SESSION_SCOPE));
+        assertEquals("contextValue", pageContext.getAttribute("contextKey", PageContext.APPLICATION_SCOPE));
+        MockHttpServletRequest otherRequest = new MockHttpServletRequest();
+        otherRequest.setAttribute("otherRequestKey", "otherRequestValue");
+        pageContext.setServletRequest(otherRequest);
+        assertEquals("otherRequestValue", pageContext.getAttribute("otherRequestKey", PageContext.REQUEST_SCOPE));
+        assertNull(pageContext.getAttribute("sessionKey", PageContext.SESSION_SCOPE));
+        pageContext.setServletConfig(null);
+        assertNull(pageContext.getAttribute("contextKey", PageContext.APPLICATION_SCOPE));
+        pageContext.setServletConfig(new MockServletConfig());
+        assertNull(pageContext.getAttribute("contextKey", PageContext.APPLICATION_SCOPE));
+        try
+        {
+            pageContext.getAttribute("contextKey", 60000);
+            fail();
+        } 
+        catch(IllegalArgumentException exc)
+        {
+            //should throw exception
+        }
+    }
+    
+    public void testSetAttribute() throws Exception
+    {
+        pageContext.setAttribute("pageKey", "pageValue");
+        pageContext.setAttribute("requestKey", "requestValue", PageContext.REQUEST_SCOPE);
+        pageContext.setAttribute("sessionKey", "sessionValue", PageContext.SESSION_SCOPE);
+        pageContext.setAttribute("contextKey", "contextValue", PageContext.APPLICATION_SCOPE);
+        assertEquals("pageValue", pageContext.getAttribute("pageKey"));
+        assertEquals("requestValue", getWebMockObjectFactory().getMockRequest().getAttribute("requestKey"));
+        assertEquals("sessionValue", getWebMockObjectFactory().getMockSession().getAttribute("sessionKey"));
+        assertEquals("contextValue", getWebMockObjectFactory().getMockServletContext().getAttribute("contextKey"));
+        pageContext.setServletRequest(null);
+        pageContext.setAttribute("requestKey1", "requestValue1", PageContext.REQUEST_SCOPE);
+        pageContext.setAttribute("sessionKey1", "sessionValue1", PageContext.SESSION_SCOPE);
+        assertNull(getWebMockObjectFactory().getMockRequest().getAttribute("requestKey1"));
+        assertNull(getWebMockObjectFactory().getMockSession().getAttribute("sessionKey1"));
+        pageContext.setServletRequest(new MockHttpServletRequest());
+        pageContext.setAttribute("sessionKey1", "sessionValue1", PageContext.SESSION_SCOPE);
+        assertNull(getWebMockObjectFactory().getMockSession().getAttribute("sessionKey1"));
+        pageContext.setServletConfig(null);
+        pageContext.setAttribute("contextKey1", "contextValue1", PageContext.APPLICATION_SCOPE);
+        assertNull(getWebMockObjectFactory().getMockServletContext().getAttribute("contextKey1"));
+        try
+        {
+            pageContext.setAttribute("requestKey", "requestValue", 60000);
+            fail();
+        } 
+        catch(IllegalArgumentException exc)
+        {
+            //should throw exception
+        }
+    }
+    
+    public void testRemoveAttribute() throws Exception
+    {
+        pageContext.setAttribute("pageKey", "pageValue");
+        pageContext.removeAttribute("pageKey", PageContext.APPLICATION_SCOPE);
+        assertNotNull(pageContext.getAttribute("pageKey"));
+        pageContext.removeAttribute("pageKey");
+        assertNull(pageContext.getAttribute("pageKey"));
+        pageContext.setAttribute("requestKey", "requestValue", PageContext.REQUEST_SCOPE);
+        pageContext.removeAttribute("requestKey", PageContext.SESSION_SCOPE);
+        assertNotNull(pageContext.getAttribute("requestKey", PageContext.REQUEST_SCOPE));
+        pageContext.removeAttribute("requestKey", PageContext.REQUEST_SCOPE);
+        assertNull(pageContext.getAttribute("requestKey", PageContext.REQUEST_SCOPE));
+        pageContext.setAttribute("sessionKey", "sessionValue", PageContext.SESSION_SCOPE);
+        pageContext.removeAttribute("sessionKey", PageContext.PAGE_SCOPE);
+        assertNotNull(pageContext.getAttribute("sessionKey", PageContext.SESSION_SCOPE));
+        pageContext.removeAttribute("sessionKey", PageContext.SESSION_SCOPE);
+        assertNull(pageContext.getAttribute("sessionKey", PageContext.SESSION_SCOPE));
+        pageContext.setAttribute("contextKey", "contextValue", PageContext.APPLICATION_SCOPE);
+        pageContext.removeAttribute("contextKey", PageContext.REQUEST_SCOPE);
+        assertNotNull(pageContext.getAttribute("contextKey", PageContext.APPLICATION_SCOPE));
+        pageContext.removeAttribute("contextKey", PageContext.APPLICATION_SCOPE);
+        assertNull(pageContext.getAttribute("contextKey", PageContext.APPLICATION_SCOPE));
+        getWebMockObjectFactory().getMockRequest().setAttribute("requestKey1", "requestValue1");
+        getWebMockObjectFactory().getMockSession().setAttribute("sessionKey1", "sessionValue1");
+        pageContext.setServletRequest(null);
+        pageContext.removeAttribute("requestKey1", PageContext.REQUEST_SCOPE);
+        pageContext.removeAttribute("sessionKey1", PageContext.SESSION_SCOPE);
+        assertNotNull(getWebMockObjectFactory().getMockRequest().getAttribute("requestKey1"));
+        assertNotNull(getWebMockObjectFactory().getMockSession().getAttribute("sessionKey1"));
+        getWebMockObjectFactory().getMockRequest().setSession(null);
+        pageContext.setServletRequest(getWebMockObjectFactory().getMockRequest());
+        pageContext.removeAttribute("requestKey1", PageContext.REQUEST_SCOPE);
+        pageContext.removeAttribute("sessionKey1", PageContext.SESSION_SCOPE);
+        assertNull(getWebMockObjectFactory().getMockRequest().getAttribute("requestKey1"));
+        assertNotNull(getWebMockObjectFactory().getMockSession().getAttribute("sessionKey1"));
+        getWebMockObjectFactory().getMockServletContext().setAttribute("contextKey1", "contextValue1");
+        pageContext.setServletConfig(null);
+        pageContext.removeAttribute("contextKey1", PageContext.APPLICATION_SCOPE);
+        assertNotNull(getWebMockObjectFactory().getMockServletContext().getAttribute("contextKey1"));
+        try
+        {
+            pageContext.removeAttribute("requestKey", 60000);
+            fail();
+        } 
+        catch(IllegalArgumentException exc)
+        {
+            //should throw exception
+        }
+    }
+    
+    public void testFindAttribute()
+    {
+        assertNull(pageContext.findAttribute("attribute"));
+        getWebMockObjectFactory().getMockServletContext().setAttribute("attribute", "contextValue");
+        assertEquals("contextValue", pageContext.findAttribute("attribute"));
+        getWebMockObjectFactory().getMockSession().setAttribute("attribute", "sessionValue");
+        assertEquals("sessionValue", pageContext.findAttribute("attribute"));
+        getWebMockObjectFactory().getMockRequest().setAttribute("attribute", "requestValue");
+        assertEquals("requestValue", pageContext.findAttribute("attribute"));
+        pageContext.setAttribute("attribute", "pageValue");
+        assertEquals("pageValue", pageContext.findAttribute("attribute"));
+        pageContext.setAttribute("attribute", null);
+        pageContext.setServletRequest(null);
+        assertEquals("contextValue", pageContext.findAttribute("attribute"));
+        pageContext.setServletConfig(null);
+        assertNull(pageContext.findAttribute("attribute"));
+    }
+    
+    public void testGetAttributesScope()
+    {
+        assertEquals(0, pageContext.getAttributesScope("attribute"));
+        getWebMockObjectFactory().getMockServletContext().setAttribute("attribute", "contextValue");
+        assertEquals(PageContext.APPLICATION_SCOPE, pageContext.getAttributesScope("attribute"));
+        getWebMockObjectFactory().getMockSession().setAttribute("attribute", "sessionValue");
+        assertEquals(PageContext.SESSION_SCOPE, pageContext.getAttributesScope("attribute"));
+        getWebMockObjectFactory().getMockRequest().setAttribute("attribute", "requestValue");
+        assertEquals(PageContext.REQUEST_SCOPE, pageContext.getAttributesScope("attribute"));
+        pageContext.setAttribute("attribute", "pageValue");
+        assertEquals(PageContext.PAGE_SCOPE, pageContext.getAttributesScope("attribute"));
+        pageContext.setAttribute("attribute", null);
+        pageContext.setServletRequest(null);
+        assertEquals(PageContext.APPLICATION_SCOPE, pageContext.getAttributesScope("attribute"));
+        pageContext.setServletConfig(null);
+        assertEquals(0, pageContext.getAttributesScope("attribute"));
+    }
+    
+    public void testGetAttributeNamesInScope()
+    {
+        getWebMockObjectFactory().getMockRequest().clearAttributes();
+        assertFalse(pageContext.getAttributeNamesInScope(PageContext.PAGE_SCOPE).hasMoreElements());
+        assertFalse(pageContext.getAttributeNamesInScope(PageContext.REQUEST_SCOPE).hasMoreElements());
+        assertFalse(pageContext.getAttributeNamesInScope(PageContext.SESSION_SCOPE).hasMoreElements());
+        assertFalse(pageContext.getAttributeNamesInScope(PageContext.APPLICATION_SCOPE).hasMoreElements());
+        pageContext.setAttribute("pageKey", "pageValue");
+        getWebMockObjectFactory().getMockRequest().setAttribute("requestKey", "requestValue");
+        getWebMockObjectFactory().getMockSession().setAttribute("sessionKey", "sessionValue");
+        getWebMockObjectFactory().getMockServletContext().setAttribute("contextKey", "contextValue");
+        Enumeration pageEnumeration = pageContext.getAttributeNamesInScope(PageContext.PAGE_SCOPE);
+        assertEquals("pageKey", pageEnumeration.nextElement());
+        assertFalse(pageEnumeration.hasMoreElements());
+        Enumeration requestEnumeration = pageContext.getAttributeNamesInScope(PageContext.REQUEST_SCOPE);
+        assertEquals("requestKey", requestEnumeration.nextElement());
+        assertFalse(requestEnumeration.hasMoreElements());
+        Enumeration sessionEnumeration = pageContext.getAttributeNamesInScope(PageContext.SESSION_SCOPE);
+        assertEquals("sessionKey", sessionEnumeration.nextElement());
+        assertFalse(sessionEnumeration.hasMoreElements());
+        Enumeration contextEnumeration = pageContext.getAttributeNamesInScope(PageContext.APPLICATION_SCOPE);
+        assertEquals("contextKey", contextEnumeration.nextElement());
+        assertFalse(contextEnumeration.hasMoreElements());
+        pageContext.setServletRequest(null);
+        assertFalse(pageContext.getAttributeNamesInScope(PageContext.REQUEST_SCOPE).hasMoreElements());
+        assertFalse(pageContext.getAttributeNamesInScope(PageContext.SESSION_SCOPE).hasMoreElements());
+        pageContext.setServletConfig(null);
+        assertFalse(pageContext.getAttributeNamesInScope(PageContext.APPLICATION_SCOPE).hasMoreElements());
+        try
+        {
+            pageContext.getAttributeNamesInScope(60000);
+            fail();
+        } 
+        catch(IllegalArgumentException exc)
+        {
+            //should throw exception
+        }
+    }
+    
+    public void testForward() throws Exception
+    {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        pageContext.setServletRequest(request);
+        pageContext.setServletResponse(response);
+        pageContext.forward("aPath");
+        MockRequestDispatcher dispatcher = (MockRequestDispatcher)request.getRequestDispatcher("aPath");
+        assertSame(request, dispatcher.getForwardedRequest());
+        assertSame(response, dispatcher.getForwardedResponse());
+    }
+    
+    public void testInclude() throws Exception
+    {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        pageContext.setServletRequest(request);
+        pageContext.setServletResponse(response);
+        pageContext.include("aPath");
+        MockRequestDispatcher dispatcher = (MockRequestDispatcher)request.getRequestDispatcher("aPath");
+        assertSame(request, dispatcher.getIncludedRequest());
+        assertSame(response, dispatcher.getIncludedResponse());
+    }
+    
+    public void testIncludeWithFlush() throws Exception
+    {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        TestJspWriter jspWriter = new TestJspWriter();
+        pageContext.setServletRequest(request);
+        pageContext.setServletResponse(response);
+        pageContext.setJspWriter(jspWriter);
+        pageContext.include("aPath", false);
+        MockRequestDispatcher dispatcher = (MockRequestDispatcher)request.getRequestDispatcher("aPath");
+        assertSame(request, dispatcher.getIncludedRequest());
+        assertSame(response, dispatcher.getIncludedResponse());
+        assertFalse(jspWriter.isFlushed());
+        pageContext.include("aPath", true);
+        assertTrue(jspWriter.isFlushed());
+    }
+    
+    private class TestJspWriter extends MockJspWriter
+    {
+        private boolean flushed = false;
+        
+        public boolean isFlushed()
+        {
+            return flushed;
+        }
+        
+        public void flush() throws IOException
+        {
+            super.flush();
+            flushed = true;
+        }
     }
 }
