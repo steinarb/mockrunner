1481f213c06f477ea1bd476c44aa48d24909a262
aibba <aibba>
2005-02-27 13:43:26 +0000

    *** empty log message ***

--- src/com/mockrunner/util/common/MethodUtil.java
+++ src/com/mockrunner/util/common/MethodUtil.java
@@ -27,4 +27,28 @@ public class MethodUtil
             throw new NestedApplicationException(exc);
         }
     }
+    
+    /**
+     * Invokes the method with the specified name on the specified object
+     * and throws a {@link com.mockrunner.base.NestedApplicationException},
+     * if the invocation fails. The method must be public and must have
+     * exactly one paremeter of the type specified by the given
+     * <code>parameter</code>.
+     * @param object the object the method is invoked from
+     * @param methodName the name of the method
+     * @param parameter the parameter, must not be <code>null</code>
+     * @return the result of the method invocation
+     */
+    public static Object invoke(Object object, String methodName, Object parameter)
+    {
+        try
+        {
+            Method method = object.getClass().getMethod(methodName, new Class[] {parameter.getClass()});
+            return method.invoke(object, new Object[] {parameter});
+        } 
+        catch(Exception exc)
+        {
+            throw new NestedApplicationException(exc);
+        }
+    }
 }
