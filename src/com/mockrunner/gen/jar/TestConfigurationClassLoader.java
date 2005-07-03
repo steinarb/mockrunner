7615f0914fbd41f337408a3ec3dc28aeb39d9c0a
aibba <aibba>
2005-07-03 21:00:50 +0000

    *** empty log message ***

--- src/com/mockrunner/gen/jar/TestConfigurationClassLoader.java
+++ src/com/mockrunner/gen/jar/TestConfigurationClassLoader.java
@@ -24,16 +24,17 @@ public class TestConfigurationClassLoader extends URLClassLoader
         catch(ClassNotFoundException exc)
         {
             /*
-             * Dirty hack to get around CGLib 2.0.2 issues.
+             * Dirty hack to get around CGLib 2.0.2 (and up) issues.
              * CGLib tries to load some internal enhanced classes
              * with the classloader before recreating them. The system 
              * classloader returns the enhanced version of the previous 
-             * run which causes a ClassCastException.
+             * run which causes a ClassCastException. The ClassNotFoundException
+             * forces CGLib to recreate this classes.
              * This is not necessary for CGGLIB 2.0 and previous.
              */
             if(name.indexOf("ByCGLIB$") != -1)
             {
-                throw new ClassNotFoundException();
+                throw exc;
             }
             clazz = getParent().loadClass(name);
         }
