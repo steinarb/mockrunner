0dc4931f7a6c13222ac19f3d7db9f71685d7c61e
aibba <aibba>
2005-07-09 19:55:20 +0000

    *** empty log message ***

--- src/com/mockrunner/connector/StreamableByteArrayInteraction.java
+++ src/com/mockrunner/connector/StreamableByteArrayInteraction.java
@@ -15,6 +15,19 @@ import com.mockrunner.mock.connector.cci.MockStreamableByteArrayRecord;
 import com.mockrunner.util.common.ArrayUtil;
 import com.mockrunner.util.common.StreamUtil;
 
+/**
+ * This interaction implementor works with bytes arrays and streamable
+ * records. It takes a byte array for the request and a byte array or
+ * a record instance as a response. If the request byte array is <code>null</code>,
+ * which is the default, the implementor accepts any request and returns the specified
+ * result. If a request byte array is specified, this implementor accepts only 
+ * requests that are equal to the specified request byte array. In this case 
+ * the actual request <code>Record</code> must implement <code>Streamable</code>.
+ * If a response record is specified, it always takes precedence, i.e.
+ * the {@link #execute(InteractionSpec , Record)} method returns this
+ * response record regardless of any byte array response. Please check
+ * out the documentation of the various methods for details.
+ */
 public class StreamableByteArrayInteraction implements InteractionImplementor
 {
     private byte[] expectedRequest;
@@ -103,7 +116,6 @@ public class StreamableByteArrayInteraction implements InteractionImplementor
             this.responseData = (byte[])ArrayUtil.copyArray(responseData);
         }
         this.responseClass = responseClass;
-        this.responseRecord = null;
     }
 
     public void setResponse(InputStream responseData)
@@ -126,16 +138,31 @@ public class StreamableByteArrayInteraction implements InteractionImplementor
             this.responseData = StreamUtil.getStreamAsByteArray(responseData);
         }
         this.responseClass = responseClass;
-        this.responseRecord = null;
     }
     
     public void setResponse(Record responseRecord)
     {
         this.responseRecord = responseRecord;
-        this.responseData = null;
-        this.responseClass = MockStreamableByteArrayRecord.class;
     }
     
+    /**
+     * Returns <code>true</code> if this implementor will handle the request.
+     * This method returns <code>true</code> in the following cases:<br><br>
+     * The response <code>Record</code> must implement <code>Streamable</code> 
+     * or it must be <code>null</code> (which is the case, if the actual request 
+     * targets the {@link #execute(InteractionSpec,Record)} method instead of 
+     * {@link #execute(InteractionSpec,Record,Record)})<br><br>
+     * The expected request must be <code>null</code> (use the various
+     * <code>setExpectedRequest</code> methods) or the actual request <code>Record</code>
+     * must implement <code>Streamable</code> and must contain the same data as
+     * the specified expected request.<br><br>
+     * Otherwise, <code>false</code> is returned.
+     * @param interactionSpec the <code>InteractionSpec</code> for the actual call
+     * @param request the request for the actual call
+     * @param response the response for the actual call, may be <code>null</code>
+     * @return <code>true</code> if this implementor will handle the request and
+     *         will return the specified response, <code>false</code> otherwise
+     */
     public boolean canHandle(InteractionSpec interactionSpec, Record request, Record response)
     {
         if(!isReponseAcceptable(response)) return false;
