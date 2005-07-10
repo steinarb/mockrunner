a924649d1d97058de06d9534b57af28c15595a50
aibba <aibba>
2005-07-10 10:40:03 +0000

    *** empty log message ***

--- src/com/mockrunner/connector/StreamableByteArrayInteraction.java
+++ src/com/mockrunner/connector/StreamableByteArrayInteraction.java
@@ -18,15 +18,12 @@ import com.mockrunner.util.common.StreamUtil;
 /**
  * This interaction implementor works with bytes arrays and streamable
  * records. It takes a byte array for the request and a byte array or
- * a record instance as a response. If the request byte array is <code>null</code>,
- * which is the default, the implementor accepts any request and returns the specified
- * result. If a request byte array is specified, this implementor accepts only 
- * requests that are equal to the specified request byte array. In this case 
- * the actual request <code>Record</code> must implement <code>Streamable</code>.
- * If a response record is specified, it always takes precedence, i.e.
- * the {@link #execute(InteractionSpec , Record)} method returns this
- * response record regardless of any byte array response. Please check
- * out the documentation of the various methods for details.
+ * a <code>Record</code> instance for the response. If the request byte array 
+ * is <code>null</code>, which is the default, the implementor accepts any 
+ * request and returns the specified result. If a request byte array is specified, 
+ * this implementor accepts only requests that are equal to the specified request 
+ * byte array. If a request is accepted, this implementor replies with the specified
+ * response. Please check out the documentation of the various methods for details.
  */
 public class StreamableByteArrayInteraction implements InteractionImplementor
 {
@@ -148,7 +145,7 @@ public class StreamableByteArrayInteraction implements InteractionImplementor
     /**
      * Returns <code>true</code> if this implementor will handle the request.
      * This method returns <code>true</code> in the following cases:<br><br>
-     * The response <code>Record</code> must implement <code>Streamable</code> 
+     * The response <code>Record</code> must implement <code>Streamable</code>
      * or it must be <code>null</code> (which is the case, if the actual request 
      * targets the {@link #execute(InteractionSpec,Record)} method instead of 
      * {@link #execute(InteractionSpec,Record,Record)})<br><br>
@@ -158,15 +155,15 @@ public class StreamableByteArrayInteraction implements InteractionImplementor
      * the specified expected request.<br><br>
      * Otherwise, <code>false</code> is returned.
      * @param interactionSpec the <code>InteractionSpec</code> for the actual call
-     * @param request the request for the actual call
-     * @param response the response for the actual call, may be <code>null</code>
+     * @param actualRequest the request for the actual call
+     * @param actualResponse the response for the actual call, may be <code>null</code>
      * @return <code>true</code> if this implementor will handle the request and
      *         will return the specified response, <code>false</code> otherwise
      */
-    public boolean canHandle(InteractionSpec interactionSpec, Record request, Record response)
+    public boolean canHandle(InteractionSpec interactionSpec, Record actualRequest, Record actualResponse)
     {
-        if(!isReponseAcceptable(response)) return false;
-        return doesRequestMatch(request);
+        if(!isResponseAcceptable(actualResponse)) return false;
+        return doesRequestMatch(actualRequest);
     }
     
     private boolean doesRequestMatch(Record request)
@@ -191,7 +188,7 @@ public class StreamableByteArrayInteraction implements InteractionImplementor
         return false;
     }
     
-    private boolean isReponseAcceptable(Record response)
+    private boolean isResponseAcceptable(Record response)
     {
         return (null == response) || (response instanceof Streamable);
     }
@@ -201,9 +198,33 @@ public class StreamableByteArrayInteraction implements InteractionImplementor
         return (null == responseClass) || ((Streamable.class.isAssignableFrom(responseClass)) && (Record.class.isAssignableFrom(responseClass)));
     }
     
-    public Record execute(InteractionSpec interactionSpec, Record request) throws ResourceException
+    /**
+     * First version of the <code>execute</code> methods.<br><br>
+     * This method returns <code>null</code>, if the request does not match 
+     * according to the contract of {@link #canHandle}. This never happens under 
+     * normal conditions since the {@link InteractionHandler} does not call 
+     * <code>execute</code>, if {@link #canHandle} returns <code>false</code>.
+     * <br><br>
+     * Otherwise, this method returns the specified reponse. If a response 
+     * <code>Record</code> object is specified (use {@link #setResponse(Record)}), 
+     * it always takes precedence, i.e. the byte array response will be ignored.
+     * If no <code>Record</code> object is specified, a <code>Record</code> object
+     * is created and filled with the specified byte response data. Use the 
+     * <code>setResponse</code> methods that take a byte array or an <code>InputStream</code>
+     * to prepare response data. The created <code>Record</code> is of the the
+     * specified type (the <code>setResponse</code> methods that take  a second
+     * <code>Class</code> parameter allows for specifying a type). If no type
+     * is specified, a {@link com.mockrunner.mock.connector.cci.MockStreamableByteArrayRecord}
+     * is created. If no response data is specified at all, an empty
+     * {@link com.mockrunner.mock.connector.cci.MockStreamableByteArrayRecord}
+     * will be returned.
+     * @param interactionSpec the interaction spec
+     * @param actualRequest the actual request
+     * @return the response according to the current request
+     */
+    public Record execute(InteractionSpec interactionSpec, Record actualRequest) throws ResourceException
     {
-        if(!canHandle(interactionSpec, request, null)) return null;
+        if(!canHandle(interactionSpec, actualRequest, null)) return null;
         if(null != responseRecord) return responseRecord;
         Streamable response = null;
         try
@@ -230,14 +251,34 @@ public class StreamableByteArrayInteraction implements InteractionImplementor
         return (Record)response;
     }
 
-    public boolean execute(InteractionSpec interactionSpec, Record request, Record response) throws ResourceException
+    /**
+     * Second version of the <code>execute</code> methods.<br><br>
+     * This method returns <code>false</code>, if the request does not match 
+     * according to the contract of {@link #canHandle}. This never happens under 
+     * normal conditions since the {@link InteractionHandler} does not call 
+     * <code>execute</code>, if {@link #canHandle} returns <code>false</code>.
+     * <br><br>
+     * Otherwise, this method fills the reponse <code>Record</code> with the
+     * specified byte response data. Use the <code>setResponse</code> methods that 
+     * take a byte array or an <code>InputStream</code> to prepare response data. 
+     * The response <code>Record</code> must implement <code>Streamable</code>
+     * (it does, otherwise the request would have been rejected by
+     * {@link #canHandle}). If no response data is specified at all,
+     * the response <code>Record</code> is not touched but <code>true</code>
+     * is returned anyway
+     * @param interactionSpec the interaction spec
+     * @param actualRequest the actual request
+     * @param actualResponse the actual response
+     * @return <code>true</code> under normal conditions
+     */
+    public boolean execute(InteractionSpec interactionSpec, Record actualRequest, Record actualResponse) throws ResourceException
     {
-        if(!canHandle(interactionSpec, request, response)) return false;
+        if(!canHandle(interactionSpec, actualRequest, actualResponse)) return false;
         try
         {
-            if(null != responseData && null != response)
+            if(null != responseData && null != actualResponse)
             {
-                ((Streamable)response).read(new ByteArrayInputStream(responseData));
+                ((Streamable)actualResponse).read(new ByteArrayInputStream(responseData));
             }
         } 
         catch(Exception exc)
