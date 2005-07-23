b96ae60fc035fa46e143cd0b6c92b9021d19477f
aibba <aibba>
2005-07-23 17:07:38 +0000

    *** empty log message ***

--- src/com/mockrunner/connector/InteractionHandler.java
+++ src/com/mockrunner/connector/InteractionHandler.java
@@ -11,47 +11,47 @@ public class InteractionHandler
 {
 	private Vector implementors = null;
 
-	public InteractionHandler() 
+    public InteractionHandler()
     {
-		implementors = new Vector();
-	}
-	
-	public void addImplementor(InteractionImplementor ii)
+        implementors = new Vector();
+    }
+
+    public void addImplementor(InteractionImplementor ii)
     {
-		implementors.add(ii);
-	}
-    
+        implementors.add(ii);
+    }
+
     public void clearImplementors()
     {
         implementors.clear();
     }
-	
+
     public Record execute(InteractionSpec is, Record request) throws ResourceException
     {
         Iterator iter = implementors.iterator();
         while (iter.hasNext())
         {
-            InteractionImplementor ii = (InteractionImplementor)iter.next();
-            if(ii.canHandle(is, request, null))
+            InteractionImplementor ii = (InteractionImplementor) iter.next();
+            if (ii.canHandle(is, request, null))
             {
                 return ii.execute(is, request);
             }
         }
         return null;
     }
-    
-    public boolean execute(InteractionSpec is, Record request, Record response) throws ResourceException 
+
+    public boolean execute(InteractionSpec is, Record request, Record response) throws ResourceException
     {
-		Iterator iter = implementors.iterator();
-		while (iter.hasNext())
+        Iterator iter = implementors.iterator();
+        while (iter.hasNext())
         {
-			InteractionImplementor ii = (InteractionImplementor)iter.next();
-            if(ii.canHandle(is, request, response))
+            InteractionImplementor ii = (InteractionImplementor) iter.next();
+            if (ii.canHandle(is, request, response))
             {
                 return ii.execute(is, request, response);
             }
-		}
-		// do I need to throw here?
-		return false;
-	}
+        }
+        // do I need to throw here?
+        return false;
+    }
 }
