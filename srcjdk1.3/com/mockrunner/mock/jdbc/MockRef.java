5df7e5b207ebbb13ec78233878a745942baeded9
aibba <aibba>
2005-10-01 15:41:06 +0000

    *** empty log message ***

--- srcjdk1.3/com/mockrunner/mock/jdbc/MockRef.java
+++ srcjdk1.3/com/mockrunner/mock/jdbc/MockRef.java
@@ -51,6 +51,26 @@ public class MockRef implements Ref, Cloneable
         this.object = object;
     }
     
+    public boolean equals(Object obj)
+    {
+        if(null == obj) return false;
+        if(!obj.getClass().equals(this.getClass())) return false;
+        MockRef other = (MockRef)obj;
+        if(null != baseTypeName && !baseTypeName.equals(other.baseTypeName)) return false;
+        if(null != other.baseTypeName && !other.baseTypeName.equals(baseTypeName)) return false;
+        if(null == object && null == other.object) return true;
+        if(null == object || null == other.object) return false;
+        return object.equals(other.object);
+    }
+
+    public int hashCode()
+    {
+        int hashCode = 0;
+        if(null != baseTypeName) hashCode += 31 * baseTypeName.hashCode();
+        if(null != object) hashCode += 31 * object.hashCode();
+        return hashCode;
+    }
+    
     public String toString()
     {
         return "Ref data: " + object.toString();
