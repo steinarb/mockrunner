6e0f8459e298b0b538fffa512adbcfceee7f2209
aibba <aibba>
2005-10-25 16:10:17 +0000

    *** empty log message ***

--- src/com/mockrunner/test/jdbc/AbstractResultSetHandlerTest.java
+++ src/com/mockrunner/test/jdbc/AbstractResultSetHandlerTest.java
@@ -11,28 +11,28 @@ import com.mockrunner.mock.jdbc.MockResultSet;
 public class AbstractResultSetHandlerTest extends BaseTestCase
 {
     private MockConnection connection;
-	private StatementResultSetHandler statementHandler;
+    private StatementResultSetHandler statementHandler;
 
-	protected void setUp() throws Exception
-	{
-		super.setUp();
-		connection = getJDBCMockObjectFactory().getMockConnection();
-		statementHandler = connection.getStatementResultSetHandler();
-	}
-	
-	public void testGetResultSet()
-	{
-	    MockResultSet result = new MockResultSet("id");
-	    statementHandler.prepareResultSet("select column from table", result);
-	    statementHandler.prepareResultSet("select .*", result);
-	    assertSame(result, statementHandler.getResultSet("select column from table"));
-	    assertNull(statementHandler.getResultSet("select xyz from table"));
-	    statementHandler.setUseRegularExpressions(true);
-	    assertSame(result, statementHandler.getResultSet("select column from table"));
-	    assertSame(result, statementHandler.getResultSet("SELECT xyz from table"));
-	    statementHandler.setCaseSensitive(true);
-	    assertNull(statementHandler.getResultSet("SELECT xyz from table"));
-	}
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        connection = getJDBCMockObjectFactory().getMockConnection();
+        statementHandler = connection.getStatementResultSetHandler();
+    }
+    
+    public void testGetResultSet()
+    {
+        MockResultSet result = new MockResultSet("id");
+        statementHandler.prepareResultSet("select column from table", result);
+        statementHandler.prepareResultSet("select .*", result);
+        assertSame(result, statementHandler.getResultSet("select column from table"));
+        assertNull(statementHandler.getResultSet("select xyz from table"));
+        statementHandler.setUseRegularExpressions(true);
+        assertSame(result, statementHandler.getResultSet("select column from table"));
+        assertSame(result, statementHandler.getResultSet("SELECT xyz from table"));
+        statementHandler.setCaseSensitive(true);
+        assertNull(statementHandler.getResultSet("SELECT xyz from table"));
+    }
     
     public void testGetResultSets()
     {
@@ -65,6 +65,7 @@ public class AbstractResultSetHandlerTest extends BaseTestCase
         statementHandler.prepareGlobalResultSets(results);
         assertSame(result0, statementHandler.getGlobalResultSet());
         returnedResults = statementHandler.getGlobalResultSets();
+        assertNotSame(results, returnedResults);
         assertEquals(2, returnedResults.length);
         assertSame(result0, returnedResults[0]);
         assertSame(result1, returnedResults[1]);
@@ -84,20 +85,20 @@ public class AbstractResultSetHandlerTest extends BaseTestCase
         statementHandler.setCaseSensitive(true);
         assertNull(statementHandler.getGeneratedKeys("INSERt regular"));
     }
-	
-	public void testGetUpdateCount()
-	{
-	    statementHandler.prepareUpdateCount(".*", 3);
-	    statementHandler.prepareUpdateCount("insert xyz", 4);
-	    assertEquals(new Integer(4), statementHandler.getUpdateCount("insert xyz"));
-	    assertEquals(new Integer(3), statementHandler.getUpdateCount(".*"));
-	    statementHandler.setUseRegularExpressions(true);
-	    assertEquals(new Integer(3), statementHandler.getUpdateCount("insert xyz"));
-	    assertEquals(new Integer(3), statementHandler.getUpdateCount("insert"));
-	    statementHandler.setExactMatch(true);
-	    assertEquals(new Integer(4), statementHandler.getUpdateCount("insert Xyz"));
-	    assertNull(statementHandler.getUpdateCount("insert"));
-	}
+    
+    public void testGetUpdateCount()
+    {
+        statementHandler.prepareUpdateCount(".*", 3);
+        statementHandler.prepareUpdateCount("insert xyz", 4);
+        assertEquals(new Integer(4), statementHandler.getUpdateCount("insert xyz"));
+        assertEquals(new Integer(3), statementHandler.getUpdateCount(".*"));
+        statementHandler.setUseRegularExpressions(true);
+        assertEquals(new Integer(3), statementHandler.getUpdateCount("insert xyz"));
+        assertEquals(new Integer(3), statementHandler.getUpdateCount("insert"));
+        statementHandler.setExactMatch(true);
+        assertEquals(new Integer(4), statementHandler.getUpdateCount("insert Xyz"));
+        assertNull(statementHandler.getUpdateCount("insert"));
+    }
     
     public void testGetUpdateCounts()
     {
@@ -124,37 +125,38 @@ public class AbstractResultSetHandlerTest extends BaseTestCase
         statementHandler.prepareGlobalUpdateCounts(updateCounts);
         assertEquals(1, statementHandler.getGlobalUpdateCount());
         returnedUpdateCounts = statementHandler.getGlobalUpdateCounts();
+        assertNotSame(updateCounts, returnedUpdateCounts);
         assertEquals(2, returnedUpdateCounts.length);
         assertEquals(1, returnedUpdateCounts[0]);
         assertEquals(2, returnedUpdateCounts[1]);
     }
-	
-	public void testGetReturnsResultSet()
-	{
-	    statementHandler.prepareReturnsResultSet("select.*", false);
-	    assertNull(statementHandler.getReturnsResultSet("select abc"));
-	    statementHandler.setUseRegularExpressions(true);
-	    assertEquals(new Boolean(false), statementHandler.getReturnsResultSet("select abc"));
-	}
-	
-	public void testGetThrowsSQLException()
-	{
-	    SQLException exc = new BatchUpdateException();
+    
+    public void testGetReturnsResultSet()
+    {
+        statementHandler.prepareReturnsResultSet("select.*", false);
+        assertNull(statementHandler.getReturnsResultSet("select abc"));
+        statementHandler.setUseRegularExpressions(true);
+        assertEquals(new Boolean(false), statementHandler.getReturnsResultSet("select abc"));
+    }
+    
+    public void testGetThrowsSQLException()
+    {
+        SQLException exc = new BatchUpdateException();
         statementHandler.prepareThrowsSQLException("[abc] statement", exc);
         statementHandler.prepareThrowsSQLException("[abc] statementxyz");
-	    assertFalse(statementHandler.getThrowsSQLException("a stAtement"));
+        assertFalse(statementHandler.getThrowsSQLException("a stAtement"));
         assertNull(statementHandler.getSQLException("a stAtement"));
-	    statementHandler.setUseRegularExpressions(true);
-	    assertTrue(statementHandler.getThrowsSQLException("a stAtement"));
+        statementHandler.setUseRegularExpressions(true);
+        assertTrue(statementHandler.getThrowsSQLException("a stAtement"));
         assertSame(exc, statementHandler.getSQLException("a stAtement"));
-	    statementHandler.setCaseSensitive(true);
-	    assertFalse(statementHandler.getThrowsSQLException("a stAtement"));
+        statementHandler.setCaseSensitive(true);
+        assertFalse(statementHandler.getThrowsSQLException("a stAtement"));
         assertNull(statementHandler.getSQLException("a stAtement"));
-	    assertTrue(statementHandler.getThrowsSQLException("b statement"));
+        assertTrue(statementHandler.getThrowsSQLException("b statement"));
         assertSame(exc, statementHandler.getSQLException("b statement"));
         assertTrue(statementHandler.getThrowsSQLException("b statementxyz"));
         assertNotSame(exc, statementHandler.getSQLException("b statementxyz"));
         String message = statementHandler.getSQLException("b statementxyz").getMessage();
         assertTrue(message.indexOf("[abc] statementxyz") != -1);
-	}
+    }
 }
