27c03079834dc7aab521085e3c5f5fbe716e86b1
aibba <aibba>
2006-03-03 19:41:11 +0000

    *** empty log message ***

--- src/com/mockrunner/test/ejb/EJBTestCaseAdapterExternalJNDITest.java
+++ src/com/mockrunner/test/ejb/EJBTestCaseAdapterExternalJNDITest.java
@@ -1,7 +1,12 @@
 package com.mockrunner.test.ejb;
 
+import java.rmi.RemoteException;
 import java.util.Hashtable;
 
+import javax.ejb.CreateException;
+import javax.ejb.EJBException;
+import javax.ejb.SessionBean;
+import javax.ejb.SessionContext;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
@@ -28,5 +33,65 @@ public class EJBTestCaseAdapterExternalJNDITest extends EJBTestCaseAdapter
         assertEquals("TestObject", lookup("testName"));
         bindToContext("testName", "testValue");
         assertEquals("TestObject", lookup("testName"));
+        deploySessionBean("TestObject", TestSessionBean.class);
+        assertEquals("TestObject", lookup("testName"));
+    }
+    
+    public static class TestSessionBean implements SessionBean
+    {   
+        public void test()
+        {
+        
+        }
+        
+        public void ejbCreate() throws CreateException
+        {
+    
+        }
+        
+        public void ejbCreate(Integer testInt) throws CreateException
+        {
+
+        }
+        
+        public void ejbCreate(int testInt, Boolean testBoolean) throws CreateException
+        {
+
+        }
+        
+        public void ejbCreateWithPostfix(int testInt, Boolean testBoolean) throws CreateException
+        {
+
+        }
+        
+        public void ejbActivate() throws EJBException, RemoteException
+        {
+
+        }
+
+        public void ejbPassivate() throws EJBException, RemoteException
+        {
+
+        }
+
+        public void ejbRemove() throws EJBException, RemoteException
+        {
+
+        }
+
+        public void setSessionContext(SessionContext context) throws EJBException, RemoteException
+        {
+            
+        }
+    }
+    
+    public static interface TestSession extends javax.ejb.EJBObject
+    {
+        public void test(boolean setRollbackOnly) throws RemoteException;
+    }
+    
+    public static interface TestSessionHome extends javax.ejb.EJBHome
+    {
+        public TestSession create() throws CreateException, RemoteException;
     }
 }
