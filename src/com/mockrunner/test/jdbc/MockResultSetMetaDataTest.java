7ab4d01079b95aba473b0c23b1213f0b41761c10
aibba <aibba>
2006-12-19 21:37:26 +0000

    *** empty log message ***

--- src/com/mockrunner/test/jdbc/MockResultSetMetaDataTest.java
+++ src/com/mockrunner/test/jdbc/MockResultSetMetaDataTest.java
@@ -1,5 +1,7 @@
 package com.mockrunner.test.jdbc;
 
+import java.sql.SQLException;
+
 import com.mockrunner.mock.jdbc.MockResultSetMetaData;
 
 import junit.framework.TestCase;
@@ -18,13 +20,8 @@ public class MockResultSetMetaDataTest extends TestCase
         metaData = null;
     }
     
-    public void testSetAndGet() throws Exception
+    private void verifyMetaData() throws SQLException
     {
-        metaData.setColumnClassName(1, "ClassName");
-        metaData.setColumnCount(4);
-        metaData.setColumnTypeName(2, "TypeName");
-        metaData.setColumnDisplaySize(3, 7);
-        metaData.setSearchable(2, false);
         assertEquals("ClassName", metaData.getColumnClassName(1));
         assertEquals(Object.class.getName(), metaData.getColumnClassName(2));
         assertEquals(4, metaData.getColumnCount());
@@ -34,5 +31,24 @@ public class MockResultSetMetaDataTest extends TestCase
         assertEquals(4, metaData.getColumnDisplaySize(5));
         assertFalse(metaData.isSearchable(2));
         assertTrue(metaData.isSearchable(1));
+        assertTrue(metaData.isReadOnly(5));
+        assertFalse(metaData.isReadOnly(1));
+        assertEquals(2, metaData.getScale(1));
+        assertEquals(3, metaData.isNullable(2));
+    }
+    
+    public void testSetAndClone() throws Exception
+    {
+        metaData.setColumnClassName(1, "ClassName");
+        metaData.setColumnCount(4);
+        metaData.setColumnTypeName(2, "TypeName");
+        metaData.setColumnDisplaySize(3, 7);
+        metaData.setSearchable(2, false);
+        metaData.setReadOnly(5, true);
+        metaData.setScale(1, 2);
+        metaData.setNullable(2, 3);
+        verifyMetaData();
+        metaData = (MockResultSetMetaData)metaData.clone();
+        verifyMetaData();
     }
 }
