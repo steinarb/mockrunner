53d26a191fe09f4c732e38fabaf9f6a8f83876d8
aibba <aibba>
2007-12-30 14:31:35 +0000

    AI

--- src/com/mockrunner/test/util/AllUtilTests.java
+++ src/com/mockrunner/test/util/AllUtilTests.java
@@ -17,6 +17,7 @@ public class AllUtilTests
 		suite.addTest(new TestSuite(ClassUtilTest.class));
         suite.addTest(new TestSuite(CaseAwareMapTest.class));
         suite.addTest(new TestSuite(MethodUtilTest.class));
+        suite.addTest(new TestSuite(FieldUtilTest.class));
         suite.addTest(new TestSuite(XmlUtilTest.class));
         //$JUnit-END$
         return suite;
