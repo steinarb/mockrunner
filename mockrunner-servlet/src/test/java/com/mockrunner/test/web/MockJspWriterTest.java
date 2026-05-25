e690c0a1770e68177e5ca2edce783f564436551c
Steinar Bang <sb@dod.no>
2026-05-25 09:38:34 +0200

    Use try-with-resource to remove warning about leaked resources

--- mockrunner-servlet/src/test/java/com/mockrunner/test/web/MockJspWriterTest.java
+++ mockrunner-servlet/src/test/java/com/mockrunner/test/web/MockJspWriterTest.java
@@ -16,35 +16,37 @@ class MockJspWriterTest
     @Test
     void testWithDefaultWriter() throws Exception
     {
-        MockJspWriter writer = new MockJspWriter();
-        writer.print("test1");
-        writer.clear();
-        writer.print("test2");
-        writer.clearBuffer();
-        writer.print("test3");
-        assertEquals("test3", writer.getOutputAsString());
+        try (MockJspWriter writer = new MockJspWriter()) {
+            writer.print("test1");
+            writer.clear();
+            writer.print("test2");
+            writer.clearBuffer();
+            writer.print("test3");
+            assertEquals("test3", writer.getOutputAsString());
+        }
     }
 
     @Test
     void testWithProvidedWriter() throws Exception
     {
         StringWriter providedWriter = new StringWriter();
-        MockJspWriter writer = new MockJspWriter(providedWriter);
-        writer.print("test1");
-        try
-        {
-            writer.clear();
-            fail();
+        try (MockJspWriter writer = new MockJspWriter(providedWriter)) {
+            writer.print("test1");
+            try
+            {
+                writer.clear();
+                fail();
+            }
+            catch(IOException exc)
+            {
+                //should throw exception
+            }
+            writer.print("test2");
+            writer.clearBuffer();
+            writer.print("test3");
+            assertEquals("", writer.getOutputAsString());
+            writer.flush();
         }
-        catch(IOException exc)
-        {
-            //should throw exception
-        }
-        writer.print("test2");
-        writer.clearBuffer();
-        writer.print("test3");
-        assertEquals("", writer.getOutputAsString());
-        writer.flush();
         assertEquals("test1test2test3", providedWriter.toString());
     }
 
@@ -52,19 +54,20 @@ class MockJspWriterTest
     void testWithProvidedResponse() throws Exception
     {
         MockHttpServletResponse response = new MockHttpServletResponse();
-        MockJspWriter writer = new MockJspWriter(response);
-        writer.print("test1");
-        assertEquals("test1", writer.getOutputAsString());
-        assertEquals("test1", response.getOutputStreamContent());
-        response.getWriter().print("test2");
-        assertEquals("test1test2", writer.getOutputAsString());
-        assertEquals("test1test2", response.getOutputStreamContent());
-        writer.clear();
-        assertEquals("", writer.getOutputAsString());
-        assertEquals("", response.getOutputStreamContent());
-        writer.print("test1");
-        writer.clearBuffer();
-        assertEquals("", writer.getOutputAsString());
+        try (MockJspWriter writer = new MockJspWriter(response)) {
+            writer.print("test1");
+            assertEquals("test1", writer.getOutputAsString());
+            assertEquals("test1", response.getOutputStreamContent());
+            response.getWriter().print("test2");
+            assertEquals("test1test2", writer.getOutputAsString());
+            assertEquals("test1test2", response.getOutputStreamContent());
+            writer.clear();
+            assertEquals("", writer.getOutputAsString());
+            assertEquals("", response.getOutputStreamContent());
+            writer.print("test1");
+            writer.clearBuffer();
+            assertEquals("", writer.getOutputAsString());
+        }
         assertEquals("", response.getOutputStreamContent());
     }
 }
